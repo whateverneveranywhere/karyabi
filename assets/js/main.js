a9282a1f4ca432d5395f4494610c3263e842f8d4
Ava Walker <firstexhotic@gmail.com>
2020-01-21 19:39:57 +0330

    email and some other stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,12 @@
 $(document).ready(function() {
     $('.js-example-basic-single').select2();
 });
+
+$(document).ready(function() {
+    $('.js-example-basic-multiple').select2({
+        maximumSelectionLength: 2
+
+    });
+
+});
+
