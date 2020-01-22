88587ea62bb2dc91904ea82747886cd35befd9f4
Ava Walker <firstexhotic@gmail.com>
2020-01-22 19:16:41 +0330

    TODO : user-resume

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,10 @@ $(document).ready(function() {
 
 });
 
+$('.js-data-example-ajax').select2({
+    ajax: {
+      url: 'https://api.github.com/search/repositories',
+      dataType: 'json'
+      // Additional AJAX parameters go here; see the end of this chapter for the full code of this example
+    }
+  });
